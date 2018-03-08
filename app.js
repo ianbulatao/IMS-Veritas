939d18f6eb021d0b8db90e7fcf0c94c1d0db9b58
Ian <ian08bulatao@gmail.com>
2018-03-08 18:59:08 +0800

    changed Angular

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const express = require('express');
+const router = express.Router();
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 // require('./public/js/db.js')
 //paths for static files
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'IMS/dist/')));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //Send Views
-app.get( '/' , (req,res) => {
-  res.render(login.html);
+app.get( '*' , (req,res) => {
+  res.send(index.html);
 });
 // app.get('/home/dashboard', function(req,res){
 //   res.send(dashboard.html);
@@ -24,3 +25,5 @@ app.get( '/' , (req,res) => {
 app.listen(80, '10.10.10.135', function() {
   console.log("listening on port: 10.10.10.135");
 })
+
+module.exports = router;
